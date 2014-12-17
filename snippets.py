22a4d76e37494cf170071fd4ffa29d145172db31
Gavin <gavin.rulon@icloud.com>
2014-12-17 18:38:25 +0000

    put function creation

--- snippets.py
+++ snippets.py
@@ -2,3 +2,8 @@ import logging
 import csv
 
 logging.basicConfig(filename="output.log",level=logging.DEBUG)
+
+
+def put(name,snippet,filename):
+	""" Store a snippet with an associated name in the CSV file """
+    pass
